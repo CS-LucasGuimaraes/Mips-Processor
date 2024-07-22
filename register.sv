a2e4776f2f546d8a82dfb1c62aae92a0375f1624
lgf <lgf@cin.ufpe.br>
2024-07-22 16:44:08 -0300

    .

---
+++ register.sv
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+module register_file(
+    input clk,
+    input [4:0] reg_address1,
+    input [4:0] reg_address2,
+    input [4:0] reg_write_address, 
+    input [31:0] write_data,
+    output [31:0] read_data1,
+    output [31:0] read_data2,
+    input write);
+
+reg [31:0] registers[31:0];
+
+integer i;
+initial begin 
+    for (i = 0; i < 32; i = i + 1) begin
+       registers[i] = 32'b0;
+    end
+
+
+assign read_data1 = registers[reg_address1]; 
+assign read_data2 = registers[reg_address2]; 
+
+always@(negedge clk)
+begin
+    if(write)
+    begin
+        registers[reg_write_address] <= write_data;
+    end
+end	
+
+endmodule
